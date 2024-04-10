b39f87cdc9953c8772ae7a1b1f1e84c98d36d3de
Alessandro Lapertosa <alessandro.lapertosa@cern.ch>
2024-04-10 21:38:15 +0200

    CMSSW code style

--- DQM/SiPixelPhase1Summary/src/SiPixelPhase1Summary.cc
+++ DQM/SiPixelPhase1Summary/src/SiPixelPhase1Summary.cc
@@ -139,7 +139,8 @@ void SiPixelPhase1Summary::bookSummaries(DQMStore::IBooker& iBooker) {
     summaryMap_[name] = iBooker.book2D("pixel" + name + "Summary", "Pixel " + name + " Summary", 12, 0, 12, 4, 0, 4);
   }
   //Make the new 6 bin ROC summary
-  deadROCSummary = iBooker.book2D("deadROCSummary", "Percentage of filled ROCs (with digis) per layer/ring", 2, 0, 2, 4, 0, 4);
+  deadROCSummary =
+      iBooker.book2D("deadROCSummary", "Percentage of filled ROCs (with digis) per layer/ring", 2, 0, 2, 4, 0, 4);
   std::vector<std::string> xAxisLabelsReduced_ = {"Barrel", "Forward"};
   deadROCSummary->setAxisTitle("Subdetector", 1);
   for (unsigned int i = 0; i < xAxisLabelsReduced_.size(); i++) {
@@ -280,7 +281,7 @@ void SiPixelPhase1Summary::fillSummaries(DQMStore::IBooker& iBooker, DQMStore::I
     unsigned int lastTrendBin = 0;
     int numEmptyBins = 0;
     float sumOf5Bins = 0.;
-    std::vector<float> last5TrendBinsValue = {0.,0.,0.,0.,0.};
+    std::vector<float> last5TrendBinsValue = {0., 0., 0., 0., 0.};
     if (runOnEndLumi_) {  //Online case
       TH1* tempProfile = deadROCTrends_[trendOrder[i]]->getTH1();
 
@@ -288,26 +289,29 @@ void SiPixelPhase1Summary::fillSummaries(DQMStore::IBooker& iBooker, DQMStore::I
       //nROCs = tempProfile->GetBinContent(tempProfile->FindLastBinAbove());
 
       // New strategy, check last 5 bins and decide
-      lastTrendBin = lumiSec/10;
+      lastTrendBin = lumiSec / 10;
       if (lastTrendBin >= 5) {
-	// Fill vector with value of last 5 bins of deadROCTrend
+        // Fill vector with value of last 5 bins of deadROCTrend
         for (unsigned int n = 0; n < 5; n++)
-	  last5TrendBinsValue[n]=tempProfile->GetBinContent(lastTrendBin-5+n+1);
+          last5TrendBinsValue[n] = tempProfile->GetBinContent(lastTrendBin - 5 + n + 1);
       } else {
-	// Fill vector with bins from 1 to last available bin
-	for (unsigned int n = 0; n < lastTrendBin; n++)
-          last5TrendBinsValue[n]=tempProfile->GetBinContent(n+1);
+        // Fill vector with bins from 1 to last available bin
+        for (unsigned int n = 0; n < lastTrendBin; n++)
+          last5TrendBinsValue[n] = tempProfile->GetBinContent(n + 1);
       }
 
-      for (unsigned int n = 0; n < 5; n++) if(last5TrendBinsValue[n]==0) numEmptyBins++;
+      for (unsigned int n = 0; n < 5; n++)
+        if (last5TrendBinsValue[n] == 0)
+          numEmptyBins++;
       if (numEmptyBins == 0) {
-	// If the last 5 bins are filled: take the median value (to suppress effect of peaks)
-	std::sort(last5TrendBinsValue.begin(), last5TrendBinsValue.end());
-	numDeadROCs = last5TrendBinsValue[2];
+        // If the last 5 bins are filled: take the median value (to suppress effect of peaks)
+        std::sort(last5TrendBinsValue.begin(), last5TrendBinsValue.end());
+        numDeadROCs = last5TrendBinsValue[2];
       } else if (numEmptyBins != 5) {
-	//If there are some empty bins: take the average of the non-empty bins
-	for (unsigned int n = 0; n < 5; n++) sumOf5Bins += last5TrendBinsValue[n];
-	numDeadROCs = sumOf5Bins / (5-numEmptyBins) ;
+        //If there are some empty bins: take the average of the non-empty bins
+        for (unsigned int n = 0; n < 5; n++)
+          sumOf5Bins += last5TrendBinsValue[n];
+        numDeadROCs = sumOf5Bins / (5 - numEmptyBins);
       }
 
     } else {  //Offline case
@@ -317,7 +321,7 @@ void SiPixelPhase1Summary::fillSummaries(DQMStore::IBooker& iBooker, DQMStore::I
     // Filled ROCs = Total number - dead ROCs
     numFilledROCs = nRocsPerTrend[i] - numDeadROCs;
     //Fill with fraction of filled ROCs (with digis)
-    fracFilledROCs =  numFilledROCs / nRocsPerTrend[i];
+    fracFilledROCs = numFilledROCs / nRocsPerTrend[i];
     deadROCSummary->setBinContent(xBin, yBin, fracFilledROCs);
     deadROCSummary->setBinContent(2, 3, -1);
     deadROCSummary->setBinContent(2, 4, -1);
@@ -367,8 +371,8 @@ void SiPixelPhase1Summary::fillSummaries(DQMStore::IBooker& iBooker, DQMStore::I
         if (i == 1 && j > 1) {
           summaryMap_["Grand"]->setBinContent(i + 1, j + 1, -1);
         } else {
-	  //Copy dead ROC Summary into Online Pixel Summary
-	  summaryMap_["Grand"]->setBinContent(i + 1, j + 1, deadROCSummary->getBinContent(i + 1, j + 1));
+          //Copy dead ROC Summary into Online Pixel Summary
+          summaryMap_["Grand"]->setBinContent(i + 1, j + 1, deadROCSummary->getBinContent(i + 1, j + 1));
 
           sumOfNonNegBins += summaryMap_["Grand"]->getBinContent(i + 1, j + 1);
         }
